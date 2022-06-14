be98a73a8743139f3e63db2e425fffc78c9f2fff
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2022-06-13 22:33:25 -0300

    arrumando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Linkin park</title>
-    <link rel="stylesheet" href="{{URL::asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('css/app.css') }}">
     
 
     <style>
